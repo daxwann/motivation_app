ce43740e865d2d1fccc9ccd0e1702364cc3edfe7
Dax Wann <daxwann@gmail.com>
2019-06-06 17:46:25 -0700

    test users controller

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,7 @@
 module ApplicationHelper
+  def auth_token
+    "<input type='hidden'
+      name='authentication_token'
+      value='#{ h(form_authentication_token) }'>".html_safe
+  end
 end
--- spec/controllers/sessions_controller_spec.rb
+++ spec/controllers/sessions_controller_spec.rb
@@ -2,11 +2,25 @@ require 'rails_helper'
 
 RSpec.describe SessionsController, type: :controller do
   describe "GET :new" do
+    it "renders :new template" do
+      get :new
+      expect(response).to render_template(:new)
+    end
   end
 
   describe "POST :create" do
+    context "signin with wrong params" do
+    end
+
+    context "signin with right params" do
+    end
   end
 
   describe "DELETE :destroy" do
+    it 'logs out current user' do
+    end
+
+    it 'redirects to signin page' do
+    end
   end
 end
