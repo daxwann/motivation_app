9808e6fa90e85e2e82198b4dc3ef32339e4be7fd
Dax Wann <daxwann@gmail.com>
2019-06-07 10:49:51 -0700

    add end-to-end test for users

---
+++ spec/features/authentication_features.rb
@@ -0,0 +1,27 @@
+require 'rails_helper'
+
+feature 'sign up process' do
+  scenario 'has new user page' do
+  end
+
+  feature 'creates a new user' do
+    scenario 'invalidates wrong params' do
+    end
+
+    scenario 'creates new user with correct params' do
+    end
+  end
+end
+
+feature 'sign in process' do
+  scenario 'has sign in page' do
+  end
+
+  feature 'signing in to new session' do
+    scenario 'invalidates wrong params' do
+    end
+
+    scenario 'creates new session with correct params' do
+    end
+  end
+end
