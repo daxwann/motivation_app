04feac931c372e48c00eff524d1b6091ebd22353
Dax Wann <daxwann@gmail.com>
2019-06-09 23:42:53 -0700

    add test for goals controller

--- app/controllers/goals_controller.rb
+++ app/controllers/goals_controller.rb
@@ -26,9 +26,10 @@ class GoalsController < ApplicationController
   def show
     @goal = Goal.find_by(id: params[:id])
 
-    if @goal && current_user.id == @goal.user_id 
+    if @goal && (@goal.public == true || current_user.id == @goal.user_id) 
       render :show
     else
+      flash[:errors] = ["You don't have permission to see this goal"]
       redirect_to user_url(current_user)
     end
   end
@@ -39,19 +40,39 @@ class GoalsController < ApplicationController
     if @goal && current_user.id == @goal.user_id
       render :edit
     else
+      flash[:errors] = ["You don't have permission to edit this goal"]
       redirect_to user_url(current_user)
     end
   end
 
   def update
+    @goal = Goal.find_by(id: params[:id])
+    
+    unless @goal.user_id == current_user.id
+      redirect_to goals_url 
+    end
+
+    if @goal.update_attributes(goal_params)
+      redirect_to goal_url(@goal)
+    else
+      flash.now[:errors] = @goal.errors.full_messages
+      render :edit
+    end
   end
 
   def destroy
+    @goal = Goal.find_by(id: params[:id])
+
+    if @goal.user_id == current_user.id
+      @goal.destroy!
+    end
+
+    redirect_to goals_url
   end
 
   private
 
   def goal_params
-    params.require(:goal).permit(:title, :detail, :public)
+    params.require(:goal).permit(:title, :detail, :public, :completed)
   end
 end
--- spec/controllers/goals_controller_spec.rb
+++ spec/controllers/goals_controller_spec.rb
@@ -2,8 +2,8 @@ require 'rails_helper'
 
 RSpec.describe GoalsController, type: :controller do
   before(:each) do
-    user = FactoryBot.create(:user)
-    request.session[:session_token] = user.session_token
+    @user = FactoryBot.create(:user) 
+    request.session[:session_token] = @user.session_token
   end
 
   describe 'GET :index' do
@@ -22,32 +22,81 @@ RSpec.describe GoalsController, type: :controller do
 
   describe 'POST :create' do
     context 'submit with incomplete params' do
+      it 'renders :new' do
+        post :create, params: { goal: { title: "", detail: "" } }
+        expect(response).to render_template("new")
+        expect(flash[:errors]).to be_present
+      end
     end
 
     context 'submit with all params' do
+      it 'redirects to goal show page' do
+        post :create, params: { 
+          goal: {
+            title: "finish appacademy",
+            detail: "Learn ruby, rails, sql, and javascript"
+          }
+        }
+        expect(response).to redirect_to(goal_url(Goal.find_by(title: "finish appacademy")))
+      end
     end
   end
 
   describe 'GET :show' do
+    subject(:goal) { FactoryBot.create(:goal, user_id: @user.id) }
     it 'renders :show template' do
+      get :show, params: { id: goal.id }
+      expect(response).to render_template("show") 
     end
   end
 
   describe 'GET :edit' do
+    subject(:goal) { FactoryBot.create(:goal, user_id: @user.id) }
     it 'renders :edit template' do
+      get :edit, params: { id: goal.id }
+      expect(response).to render_template("edit")
     end
   end
 
-  describe 'POST :update' do
+  describe 'PATCH :update' do
+    subject(:goal) { FactoryBot.create(:goal, user_id: @user.id) }
     context 'submit with incomplete params' do
+      it 're-renders edit page' do
+        patch :update, params: {
+          id: goal.id,
+          goal: {
+            title: "",
+            detail: ""
+         }
+        }
+        expect(response).to render_template("edit")
+        expect(flash[:errors]).to be_present
+      end
     end
 
     context 'submit with all params' do
+      it 'redirects to goal page' do
+        patch :update, params: {
+          id: goal.id,
+          goal: {
+            title: "finish appacademy",
+            details: "learn ruby, sql, rails, javascript",
+            completed: true,
+            public: false
+          }
+        }
+        expect(response).to redirect_to(goal_url(goal.id))
+      end
     end
-  end
+  end 
 
   describe 'DELETE :destroy' do
+    subject(:goal) { FactoryBot.create(:goal, user_id: @user.id) }
     it 'redirects to goals index' do
+      id = goal.id
+      delete :destroy, params: { id: id }
+      expect(response).to redirect_to(goals_url)
+      expect(Goal.exists?(id: id)).to be false
     end
   end
 end
