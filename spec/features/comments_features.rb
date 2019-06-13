5ed4656878be28faa0e146cc4c61081be68e0a54
Dax Wann <daxwann@gmail.com>
2019-06-12 17:37:22 -0700

    update feature test for comments

--- spec/features/comments_features.rb
+++ spec/features/comments_features.rb
@@ -10,38 +10,26 @@ feature 'comments' do
   feature 'commenting on goals page' do
     let(:goal) { FactoryBot.create(:goal, user_id: user.id) }
 
-    scenario 'add comment' do
+    scenario 'add and delete comment' do
       visit goal_url(goal)
       fill_in "New Comment", with: "This is the comment's body"
       click_button("Add Comment")
-      expect(page).to have_content("This is the comment's body") 
-    end
-
-    scenario 'delete comment' do
-      goal_comment = FactoryBot.create(:goal_comment, goal_id: goal.id, author_id: user.id)
-      visit goal_url(goal)
-      expect(page).to have_content(goal_comment.body)
+      expect(page).to have_content("This is the comment's body")
       click_button("Delete Comment")
-      expect(page).to_not have_content(goal_comment.body)
+      expect(page).to_not have_content("This is the comment's body")
     end
   end
   
   feature 'commenting on users' do
     let(:user2) { FactoryBot.create(:user) }
 
-    scenario 'add comment' do
+    scenario 'add and delete comment' do
       visit user_url(user2)
       fill_in "New Comment", with: "This is the comment's body"
       click_button("Add Comment")
       expect(page).to have_content("This is the comment's body")
-    end
-
-    scenario 'delete comment' do
-      user_comment = FactoryBot.create(:user_comment, user_id: user2.id, author_id: user.id)
-      visit user_url(user2)
-      expect(page).to have_content(user_comment.body)
       click_button("Delete Comment")
-      expect(page).to_not have_content(user_comment.body)
+      expect(page).to_not have_content("This is the comment's body")
     end
   end
 end
